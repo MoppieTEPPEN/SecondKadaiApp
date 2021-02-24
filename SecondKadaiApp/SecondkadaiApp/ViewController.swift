36d811ece9a956b38f3256adbe70a7d7dac47405
MoppieTEPPEN <79398964+MoppieTEPPEN@users.noreply.github.com>
2021-02-24 19:34:53 +0900

    Add files via upload

---
+++ SecondKadaiApp/SecondkadaiApp/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SecondkadaiApp
+//
+//  Created by Moppie on 2021/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameText: UITextField!
+    
+    
+    @IBAction func handle(_ sender: Any) {
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        }
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // segueから遷移先のResultViewControllerを取得する
+            let resultViewController:ResultViewController = segue.destination as! ResultViewController
+            // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+        resultViewController.myname = nameText.text!
+            
+    }
+    
+}
+
